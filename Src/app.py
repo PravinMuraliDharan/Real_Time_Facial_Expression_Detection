ef55fc4ec1b89f9126debabfbfc572769ad1f6dd
Pravin Muralidharan <mrpravin2000@gmail.com>
2021-10-20 21:36:48 +0530

    Final Commit

--- Src/app.py
+++ Src/app.py
@@ -8,6 +8,9 @@ from PIL import Image
 import streamlit as st
 import tensorflow as tf
 import matplotlib as plt
+from keras.preprocessing.image import img_to_array
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfiguration, VideoProcessorBase, WebRtcMode
+
 
 # Navigation Bar
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
@@ -32,8 +35,7 @@ st.markdown("""
 
 
 # Getting current working directory
-cwd = os.getcwd()     
-print(cwd)             
+cwd = os.getcwd()                  
 
 # Creating Containers
 projectInfo = st.container()
@@ -83,92 +85,83 @@ We define a CNN model and compare the predicted results with given labels.""")
 # Real Time Facial Expression Detection Demo
 st.markdown("""
 ## Real-Time Demo""")
-col1, col2 = st.columns([17,5])
-with col1:
-  start_demo = st.button('Start Demo')      # Start Demo Button
-with col2:
-  stop_demo = st.button('Stop Demo')        # Stop Demo Button
-FRAME_WINDOW = st.image([])               # Defining Frame Window
-if True:
-  cap = cv2.VideoCapture(0)               # Accessing Secondary Camera
-else:
-  cap = cv2.VideoCapture(1)               # Accessing Primary Camera
+ 
+RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
 
 
 
+# load model
 path = cwd + "\haarcascade_frontalface_default.xml"   # haarcascade file
 model_path = cwd + "\Vinnet.h5"                       # Trained Model
+vinnet_model = tf.keras.models.load_model(model_path)
+faceCascade = cv2.CascadeClassifier(path)
 
-# Real-Time Demo                                      
-while start_demo:                                      # Starting Demo
-  font_scale = 1.5
-  font = cv2.FONT_HERSHEY_DUPLEX
-  ret, frame = cap.read()
-  # Loading trained model and haarcasade
-  vinnet_model = tf.keras.models.load_model(model_path)
-  faceCascade = cv2.CascadeClassifier(path)
-  
-  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-  faces = faceCascade.detectMultiScale(gray, 1.1,4)
-  for x,y,w,h in faces:
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = frame[y:y+h, x:x+w]
-    cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 255, 255), 2)
-    faces = faceCascade.detectMultiScale(roi_gray)
-    
-  final_image = cv2.resize(roi_color, (224,224))
-  final_image = np.expand_dims(final_image, axis = 0)
-  final_image = final_image/255.0
-  Predictions = vinnet_model(final_image)                            # Prediction
-  # Status = Angry
-  if (np.argmax(Predictions) == 0):
-    status = "Angry"
-    x1,y1,w1,h1 = 0,0,175,75
-    cv2.putText(frame, status, (100, 150), font, 1.5, (0,0,255),2,cv2.LINE_4)
-    cv2.rectangle(frame, (x,y), (x+w, y+h), (255,255,255), 2)
-  # Status = Disgust
-  elif (np.argmax(Predictions) == 1):
-    status = "Disgust"
-    x1,y1,w1,h1 = 0,0,175,75
-    cv2.putText(frame, status, (100, 150), font, 1.5, (0,128,128),2,cv2.LINE_4)
-    cv2.rectangle(frame, (x,y), (x+w, y+h), (255,255,255), 2)
-  # Status = Fear
-  elif (np.argmax(Predictions) == 2):
-    status = "Fear"
-    x1,y1,w1,h1 = 0,0,175,75
-    cv2.putText(frame, status, (100, 150), font, 1.5, (0,0,128),2,cv2.LINE_4)
-    cv2.rectangle(frame, (x,y), (x+w, y+h), (255,255,255), 2)
-  # Status = Happy
-  elif (np.argmax(Predictions) == 3):
-    status = "Happy"
-    x1,y1,w1,h1 = 0,0,175,75
-    cv2.putText(frame, status, (100, 150), font, 1.5, (0,255,0),2,cv2.LINE_4)
-    cv2.rectangle(frame, (x,y), (x+w, y+h), (255,255,255), 2)
-  # Status = Sad
-  elif (np.argmax(Predictions) == 5):
-    status = "Sad"
-    x1,y1,w1,h1 = 0,0,175,75
-    cv2.putText(frame, status, (100, 150), font, 1.5, (255,0,0),2,cv2.LINE_4)
-    cv2.rectangle(frame, (x,y), (x+w, y+h), (255,255,255), 2)
-  # Status = Surprise
-  elif (np.argmax(Predictions) == 6):
-    status = "Surprise"
-    x1,y1,w1,h1 = 0,0,175,75
-    cv2.putText(frame, status, (100, 150), font, 1.5, (0,255,255),2,cv2.LINE_4)
-    cv2.rectangle(frame, (x,y), (x+w, y+h), (255,255,255), 2)
-  # Status = Neutral
-  else:
-    status = "Neutral"
-    x1,y1,w1,h1 = 0,0,175,75
-    cv2.putText(frame, status, (100, 150), font, 1.5, (255,255,255),2,cv2.LINE_4)
-    cv2.rectangle(frame, (x,y), (x+w, y+h), (255,255,255), 2)
 
-  frames = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)    # Converting BGR to RGB
-  FRAME_WINDOW.image(frames)                         # Showing results in the frame window
-  
+class Face_Expression_Detection(VideoTransformerBase):
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        font_scale = 1.5
+        font = cv2.FONT_HERSHEY_DUPLEX
 
-if stop_demo:                                        # Stopping the demo
-  cap.release()                                      # Releasing the camera
-  cv2.destroyAllWindows()
+        #image gray
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(image=img_gray, scaleFactor=1.1, minNeighbors=4)
+        for (x, y, w, h) in faces:
+            roi_gray = img_gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
+            cv2.rectangle(img, (x,y), (x+w, y+h), (255, 255, 255), 2)
+               
+        final_image = cv2.resize(roi_color, (224,224))
+        final_image = np.expand_dims(final_image, axis = 0)
+        final_image = final_image/255.0
+        Predictions = vinnet_model(final_image)                            # Prediction
+        # Status = Angry
+        if (np.argmax(Predictions) == 0):
+          status = "Angry"
+          x1,y1,w1,h1 = 0,0,175,75
+          cv2.putText(img, status, (100, 150), font, 1.5, (0,0,255),2,cv2.LINE_4)
+          cv2.rectangle(img, (x,y), (x+w, y+h), (255,255,255), 2)
+        # Status = Disgust
+        elif (np.argmax(Predictions) == 1):
+          status = "Disgust"
+          x1,y1,w1,h1 = 0,0,175,75
+          cv2.putText(img, status, (100, 150), font, 1.5, (0,128,128),2,cv2.LINE_4)
+          cv2.rectangle(img, (x,y), (x+w, y+h), (255,255,255), 2)
+        # Status = Fear
+        elif (np.argmax(Predictions) == 2):
+          status = "Fear"
+          x1,y1,w1,h1 = 0,0,175,75
+          cv2.putText(img, status, (100, 150), font, 1.5, (0,0,128),2,cv2.LINE_4)
+          cv2.rectangle(img, (x,y), (x+w, y+h), (255,255,255), 2)
+        # Status = Happy
+        elif (np.argmax(Predictions) == 3):
+          status = "Happy"
+          x1,y1,w1,h1 = 0,0,175,75
+          cv2.putText(img, status, (100, 150), font, 1.5, (0,255,0),2,cv2.LINE_4)
+          cv2.rectangle(img, (x,y), (x+w, y+h), (255,255,255), 2)
+        # Status = Sad
+        elif (np.argmax(Predictions) == 5):
+          status = "Sad"
+          x1,y1,w1,h1 = 0,0,175,75
+          cv2.putText(img, status, (100, 150), font, 1.5, (255,0,0),2,cv2.LINE_4)
+          cv2.rectangle(img, (x,y), (x+w, y+h), (255,255,255), 2)
+        # Status = Surprise
+        elif (np.argmax(Predictions) == 6):
+          status = "Surprise"
+          x1,y1,w1,h1 = 0,0,175,75
+          cv2.putText(img, status, (100, 150), font, 1.5, (0,255,255),2,cv2.LINE_4)
+          cv2.rectangle(img, (x,y), (x+w, y+h), (255,255,255), 2)
+        # Status = Neutral
+        else:
+          status = "Neutral"
+          x1,y1,w1,h1 = 0,0,175,75
+          cv2.putText(img, status, (100, 150), font, 1.5, (255,255,255),2,cv2.LINE_4)
+          cv2.rectangle(img, (x,y), (x+w, y+h), (255,255,255), 2)
+
+          
+        return img
+
+
+webrtc_streamer(key="opencv-filter", mode=WebRtcMode.SENDRECV, rtc_configuration=RTC_CONFIGURATION,video_processor_factory=Face_Expression_Detection)
 
 
