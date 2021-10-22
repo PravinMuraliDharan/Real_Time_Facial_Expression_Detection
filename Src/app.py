35bf3cc85baa96ba883070960870d5f6d23842ae
Pravin Muralidharan <mrpravin2000@gmail.com>
2021-10-22 07:24:55 +0530

    update2

--- Src/app.py
+++ Src/app.py
@@ -98,8 +98,8 @@ faceCascade = cv2.CascadeClassifier(path)
 
 
 class Face_Expression_Detection(VideoTransformerBase):
-    @st.cache(allow_output_mutation=True, max_entries=20, ttl=4600)
-    def recv(self, frame):
+    #@st.cache(allow_output_mutation=True, max_entries=20, ttl=4600)
+    def transform(self, frame):
         img = frame.to_ndarray(format="bgr24")
         font_scale = 1.5
         font = cv2.FONT_HERSHEY_DUPLEX
